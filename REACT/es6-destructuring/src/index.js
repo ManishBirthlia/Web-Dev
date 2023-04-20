9a90938607145984bbb91beef05423756fb3acaa
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2023-04-21 02:27:11 +0530

    Add files via upload

---
+++ REACT/es6-destructuring/src/index.js
@@ -0,0 +1,38 @@
+// CHALLENGE: uncomment the code below and see the car stats rendered
+import React from "react";
+import ReactDOM from "react-dom";
+import cars from "./practice";
+
+const [honda, tesla] = cars;
+const {
+  speedStats: { teslaTopSpeed }
+} = tesla;
+const {
+  speedStats: { hondaTopSpeed }
+} = honda;
+const {
+  coloursByPopularity: [teslaTopColour]
+} = tesla;
+const {
+  coloursByPopularity: [hondaTopColour]
+} = honda;
+
+ReactDOM.render(
+  <table>
+    <tr>
+      <th>Brand</th>
+      <th>Top Speed</th>
+    </tr>
+    <tr>
+      <td>{tesla.model}</td>
+      <td>{teslaTopSpeed}</td>
+      <td>{teslaTopColour}</td>
+    </tr>
+    <tr>
+      <td>{honda.model}</td>
+      <td>{hondaTopSpeed}</td>
+      <td>{hondaTopColour}</td>
+    </tr>
+  </table>,
+  document.getElementById("root")
+);
